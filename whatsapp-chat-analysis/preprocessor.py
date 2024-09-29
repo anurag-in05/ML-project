ad8d9b745c844e16af81b9b923598600ecd0c5e9
anurag <154045525+anurag-in05@users.noreply.github.com>
2024-09-29 12:11:04 +0530

    Update preprocessor.py

--- whatsapp-chat-analysis/preprocessor.py
+++ whatsapp-chat-analysis/preprocessor.py
@@ -4,16 +4,11 @@ import pandas as pd
 # used for the txt files
 def preprocess(data):
     pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
-
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
-
     df = pd.DataFrame({'user_message': messages, 'message_date': dates})
-    # convert message_date type
     df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
-
     df.rename(columns={'message_date': 'date'}, inplace=True)
-
     users = []
     messages = []
     for message in df['user_message']:
@@ -24,11 +19,60 @@ def preprocess(data):
         else:
             users.append('group_notific   ation')
             messages.append(entry[0])
+    df['user'] = users
+    df['message'] = messages
+    df.drop(columns=['user_message'], inplace=True)
+    df['only_date'] = df['date'].dt.date
+    df['year'] = df['date'].dt.year
+    df['month_num'] = df['date'].dt.month
+    df['month'] = df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+    df['day_name'] = df['date'].dt.day_name()
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
+    period = []
+    for hour in df[['day_name', 'hour']]['hour']:
+        if hour == 23:
+            period.append(str(hour) + "-" + str('00'))
+        elif hour == 0:
+            period.append(str('00') + "-" + str(hour + 1))
+        else:
+            period.append(str(hour) + "-" + str(hour + 1))
+    df['period'] = period
+    return df
 
+# Used for the txt files
+def preprocess_ios(data):
+    # Adjusted pattern for iOS format
+    pattern = r'\[\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}:\d{2}\]\s'
+    
+    # Split messages based on the pattern
+    messages = re.split(pattern, data)[1:]  # Exclude the first empty string
+    dates = re.findall(pattern, data)
+
+    # Create a DataFrame
+    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
+    df['message_date'] = df['message_date'].str.replace('[', '').str.replace(']', '').str.strip()
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %H:%M:%S', errors='raise', dayfirst=True)
+
+    df.rename(columns={'message_date': 'date'}, inplace=True)
+
+    users = []
+    messages = []
+    for message in df['user_message']:
+        entry = re.split(r'([\w\W]+?):\s', message)
+        if entry[1:]:  # User name
+            users.append(entry[1])
+            messages.append(" ".join(entry[2:]))
+        else:
+            users.append('group_notification')
+            messages.append(entry[0])
+    
     df['user'] = users
     df['message'] = messages
     df.drop(columns=['user_message'], inplace=True)
 
+    # Extract date components
     df['only_date'] = df['date'].dt.date
     df['year'] = df['date'].dt.year
     df['month_num'] = df['date'].dt.month
@@ -38,15 +82,15 @@ def preprocess(data):
     df['hour'] = df['date'].dt.hour
     df['minute'] = df['date'].dt.minute
 
+    # Create periods
     period = []
-    for hour in df[['day_name', 'hour']]['hour']:
+    for hour in df['hour']:
         if hour == 23:
-            period.append(str(hour) + "-" + str('00'))
+            period.append(f"{hour}-00")
         elif hour == 0:
-            period.append(str('00') + "-" + str(hour + 1))
+            period.append("00-01")
         else:
-            period.append(str(hour) + "-" + str(hour + 1))
-
+            period.append(f"{hour}-{hour + 1}")
+    
     df['period'] = period
-
     return df
