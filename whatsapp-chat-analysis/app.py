1d6dbef760d28a7418451c70bfc3ea36e433f20b
anurag <154045525+anurag-in05@users.noreply.github.com>
2024-09-29 12:10:24 +0530

    Update app.py

--- whatsapp-chat-analysis/app.py
+++ whatsapp-chat-analysis/app.py
@@ -11,8 +11,11 @@ uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     data = bytes_data.decode("utf-8")
-    df = preprocessor.preprocess(data)
-
+    first_line = uploaded_file.readline().decode('utf-8').strip()
+    if first_line[0] == "[":
+        df = preprocessor.preprocess_ios(data)
+    else:
+        df = preprocessor.preprocess(data)
     # fetch unique users
     user_list = df['user'].unique().tolist()
     # user_list.remove('group_notification')
